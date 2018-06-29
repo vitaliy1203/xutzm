ddce171c1149638f8e48ba0282e707c56ade1c70
Vitaliy <xutzmrabota@mail.ru>
2018-06-29 22:40:13 +0300

    pugmultipage

---
+++ source/pages/blog/blog.js
@@ -0,0 +1,4 @@
+import createMenu from '../../components/menu/menu.js';
+var menu = createMenu(['Главная','индекс'], 'menu');
+document.body.appendChild(menu);
+console.log('blog');
---
+++ source/pages/index/index.js
@@ -0,0 +1,4 @@
+import createMenu from '../../components/menu/menu.js';
+var menu = createMenu(['Главная','Блог'], 'menu');
+document.body.appendChild(menu);
+console.log('index');
