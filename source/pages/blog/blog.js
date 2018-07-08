ec5cb62c5596587e491aa54637afd8daf2022753
Vitaliy <xutzmrabota@mail.ru>
2018-07-08 18:07:00 +0300

    'normalize-css'

--- source/pages/blog/blog.js
+++ source/pages/blog/blog.js
@@ -1,4 +1,5 @@
 import './blog.scss';
+import 'normalize.css';
 
 import createMenu from '../../components/menu/menu.js';
 var menu = createMenu(['Главная','индекс'], 'menu');
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge');
 const pug = require('./webpack/pug.js');
 const devserver = require('./webpack/devserver.js');
 const sass = require('./webpack/sass.js');
+const css = require ('./webpack/css.js');
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
@@ -44,7 +45,8 @@ module.exports = function (env) {
         return merge([
             common,
             devserver(),
-            sass()
+            sass(),
+            css()
         ]);
     }
 };
